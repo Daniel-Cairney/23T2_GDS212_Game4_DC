faf6e8de42b15979bc91a5aa327ede6ad85d2a4e
Daniel-Cairney <114543733+Daniel-Cairney@users.noreply.github.com>
2023-06-29 20:39:36 +1000

    added audio

--- Assets/Scripts/ObstacleTrigger.cs
+++ Assets/Scripts/ObstacleTrigger.cs
@@ -3,11 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class ObstacleTrigger : MonoBehaviour
 {
-    
+    [SerializeField] private AudioSource audioSource;
+    //[SerializeField] private AudioClip birdDeath;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            audioSource.Play();
+                
             // Player enters the cube trigger, perform necessary actions here
             SceneManager.LoadScene("GameOverScene");
             Debug.Log("Player entered the cube trigger!");
